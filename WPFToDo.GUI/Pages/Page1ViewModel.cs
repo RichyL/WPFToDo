2354b8af7c780987ce5cc8d98063384bd3b1ec93
richy_latham@yahoo.co.uk <richy_latham@yahoo.co.uk>
2019-02-02 09:43:20 +0000

    Added some pages and set an active page using the Conductor

---
+++ WPFToDo.GUI/Pages/Page1ViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Stylet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFToDo.GUI.Pages
+{
+    public class Page1ViewModel : Screen
+    {
+    }
+}
