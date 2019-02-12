c318c9e09ce0fbe876be5035402299981c15b10f
RichyL <richylatham@gmail.com>
2019-02-12 19:28:20 +0000

    Added tests for ShowToDosViewModel.

--- WPFToDo.GUI.Tests/ViewModels/ShowToDosViewModelTests.cs
+++ WPFToDo.GUI.Tests/ViewModels/ShowToDosViewModelTests.cs
@@ -6,16 +6,58 @@ using System.Threading.Tasks;
 using WPFToDo.Database;
 using WPFToDo.GUI.ViewModels;
 using Xunit;
+using Moq;
+using WPFToDo.Common;
 
 namespace WPFToDo.GUI.Tests.ViewModels
 {
     public class ShowToDosViewModelTests
     {
-        [Fact]
-        public void GetAllToDosTest()
+
+        ShowToDosViewModel vm = null;
+
+        public ShowToDosViewModelTests()
         {
-            //ShowToDosViewModel vm = new ShowToDosViewModel(ToDoStore todoStore);
-            //vm.ToDos.
+            var toDoStoreMock = new Mock<IToDoStore>();
+            toDoStoreMock.Setup(x => x.GetAllToDos())
+                .Returns(new List<ToDo>
+                {
+                    new ToDo { Id = 1, Title = "Title 1", Description = "Description 1", Complete = false },
+                    new ToDo { Id = 2, Title = "Title 2", Description = "Description 2", Complete = false }
+                });
+
+            vm = new ShowToDosViewModel(toDoStoreMock.Object);
+        }
+
+       [Fact]
+        public void CheckCountOfToDosTest()
+        {
+         
+           vm.Load();
+
+            Assert.Equal(2, vm.ToDos.Count);
+        }
+
+        [Fact]
+        public void CheckCallLoadDoesNotLoadToDosTwice()
+        {
+
+            vm.Load();
+            vm.Load();
+
+            Assert.Equal(2, vm.ToDos.Count);
+        }
+
+        [Theory]
+        [InlineData(1,"Title 1","Description 1",false)]
+        [InlineData(2, "Title 2", "Description 2", false)]
+        public void CheckToDoDetails(int id, string title, string description, bool complete)
+        {
+            vm.Load();
+            ToDo toDo = vm.ToDos.SingleOrDefault(t => t.Id == id);
+            Assert.Equal( title, toDo.Title);
+            Assert.Equal( description, toDo.Description);
+            Assert.Equal( complete, toDo.Complete);
         }
     }
 }
--- WPFToDo.GUI/ViewModels/ShowToDosViewModel.cs
+++ WPFToDo.GUI/ViewModels/ShowToDosViewModel.cs
@@ -4,11 +4,32 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WPFToDo.Common;
+using WPFToDo.Database;
 
 namespace WPFToDo.GUI.ViewModels
 {
     public class ShowToDosViewModel : BaseViewModel, IContentViewModel
     {
+        private IToDoStore _toDoStore;
 
+        public ShowToDosViewModel(IToDoStore todoStore)
+        {
+            _toDoStore = todoStore;
+        }
+
+        private List<ToDo> _toDos;
+
+        public List<ToDo> ToDos
+        {
+            get { return _toDos; }
+            set { _toDos = value; }
+        }
+
+
+        public void Load()
+        {
+            _toDos=_toDoStore.GetAllToDos();
+        }
     }
 }
