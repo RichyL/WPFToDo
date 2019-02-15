5156ec5f465c157f7b72ca5182420e8fc4e3b11d
RichyL <richylatham@gmail.com>
2019-02-15 16:39:53 +0000

    Database tests move to Xunit. Gui tests added.

---
+++ WPFToDo.GUI.Tests/ViewModels/AddToDoViewModelTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFToDo.Database;
+using Moq;
+using WPFToDo.Common;
+using Xunit;
+using WPFToDo.GUI.ViewModels;
+
+namespace WPFToDo.GUI.Tests.ViewModels
+{
+    public class AddToDoViewModelTests 
+    {
+        AddToDoViewModel vm = null;
+
+        Mock<IToDoStore> toDoStoreMock=null;
+
+        public AddToDoViewModelTests()
+        {
+            toDoStoreMock = new Mock<IToDoStore>();
+            toDoStoreMock.Setup(x => x.AddToDo(It.IsAny<string>(), It.IsAny<string>())).
+                Returns(new ToDo { Id = 1, Title = "Title 1", Description = "Description 1" });
+
+            vm = new AddToDoViewModel(toDoStoreMock.Object);
+        }
+
+        [Fact]
+        public void CheckAddNewToDo()
+        {
+            
+            vm.Title = "Title 1";
+            vm.Description = "Description 1";
+            ToDo t = vm.SaveToDo();
+            Assert.Equal(1, t.Id);
+            Assert.Equal("Title 1", t.Title);
+            Assert.Equal("Description 1",t.Description);
+        }
+
+
+        [Fact]
+        public void CheckAddNewToDoMustHaveTitle()
+        {
+            vm.Title = "";
+            vm.Description = "Some description";
+            ArgumentException ex = Assert.Throws<ArgumentException>(() => vm.SaveToDo());
+            Assert.Equal("The title cannot be blank", ex.Message);
+        }
+    }
+}
---
+++ WPFToDo.GUI/ViewModels/AddToDoViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFToDo.Common;
+using WPFToDo.Database;
+
+namespace WPFToDo.GUI.ViewModels
+{
+    public class AddToDoViewModel : BaseViewModel, IContentViewModel
+    {
+
+        protected IToDoStore _toDoStore;
+
+        private string _title = string.Empty;
+        private string _description = string.Empty;
+
+        public AddToDoViewModel(IToDoStore todoStore)
+        { 
+            _toDoStore = todoStore;
+        }
+        
+
+        public String Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public String Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+
+        public ToDo SaveToDo()
+        {
+            if (!string.IsNullOrEmpty(_title))
+            {
+                return _toDoStore.AddToDo(_title, _description);
+                
+            }
+            else
+            {
+                throw new ArgumentException("The title cannot be blank");
+            }
+            
+        }
+
+    }
+}
